547f156906f9a75d570b06087782462b302b65c6
Thomas Geselle <thomas@cpcstrategy.com>
2017-08-28 01:58:17 -0400

    Update bootstrap.ex

--- lib/kerosene/html/bootstrap.ex
+++ lib/kerosene/html/bootstrap.ex
@@ -2,18 +2,20 @@ defmodule Kerosene.HTML.Bootstrap do
   use Phoenix.HTML
 
   def generate_links(page_list, additional_class) do
-    content_tag :nav do
-      content_tag :ul, class: build_html_class(additional_class) do
-        for {label, _page, url, current} <- page_list do
-          content_tag :li, class: build_html_class(current) do
-            link "#{label}", to: url
+    content_tag :div, class: "pagination" do
+      content_tag :ul, class: "pagination__pages" do
+        content_tag :li, class: build_html_class(additional_class) do
+          for {label, _page, url, current} <- page_list do
+            content_tag :div, class: build_html_class(current) do
+              link "#{label}", to: url
+            end
           end
         end
       end
     end
   end
 
-  defp build_html_class(true), do: "active"
+  defp build_html_class(true), do: "is-active"
   defp build_html_class(false), do: nil
   defp build_html_class(additional_class) do
     String.trim("pagination #{additional_class}")
